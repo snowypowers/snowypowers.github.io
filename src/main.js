65e35263b0eadd68c0dfbbd0e37c32c93a313986
Yak Jun Xiang <snowypowers@gmail.com>
2017-03-14 21:02:41 +0800

    Add in vue router functionality

--- src/main.js
+++ src/main.js
@@ -1,7 +1,37 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import routes from './routes.js'
+
+Vue.use(VueRouter)
+
+const scrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) {
+    // savedPosition is only available for popstate navigations.
+    return savedPosition
+  } else {
+    const position = {}
+    // new navigation.
+    // scroll to anchor by returning the selector
+    if (to.hash) {
+      position.selector = to.hash
+    } else {
+      position.x = 0
+      position.y = 0
+    }
+    return position
+  }
+}
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+  scrollBehavior
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
