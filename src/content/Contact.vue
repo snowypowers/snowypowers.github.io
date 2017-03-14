bbc1d0a92a69401bda75a668cf607891fbb12eca
Yak Jun Xiang <snowypowers@gmail.com>
2017-03-14 23:35:52 +0800

    Add Contact page
    add font awesome link for icons

---
+++ src/content/Contact.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+.flex-container.text-center#contacts
+  .flex-container#icons
+    .icon-container(v-for="(item,index) in links", :key="index")
+      i( :class="item.class", :href="item.href", aria-hidden="true")
+  p(style="align-self: flex-end") Icons taken from Font Awesome
+
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  data () {
+    return {
+      links: [
+        {class: "fa fa-github-square", href: "https://github.com/snowypowers"},
+        {class: "fa fa-linkedin-square", href: "https://www.linkedin.com/in/jun-xiang-yak-7b387687/"},
+        {class: "fa fa-envelope", href: "mailto:yakjunxiang@gmail.com"}
+      ]
+    }
+  },
+  mounted() {
+    this.$parent.$data.sections = []
+  }
+}
+</script>
+
+<style lang="stylus">
+#contacts
+  display: flex
+  height: 100%
+  flex-direction: column
+  justify-content: space-around
+  align-items: center
+  flex: 1
+
+#icons
+  width: 100%
+  flex-flow: row wrap
+  justify-content:space-around
+  align-items: center
+
+.icon-container > i.fa
+  font-size: 200px
+  margin: 30px 30px
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Home from './content/Home.vue'
 import Projects from './content/Projects.vue'
 import NotFound from './content/NotFound.vue'
+import Contact from './content/Contact.vue'
 
 const routes = [
   {path: '/', component: Home},
   {path:'/projects', component: Projects},
+  {path:'/contact', component: Contact},
   {path: '*', component: NotFound}
 ]
 
