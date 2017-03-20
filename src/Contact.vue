e1637bc2a2ad341dc353c421eafbab8383610a6e
Yak Jun Xiang <snowypowers@gmail.com>
2017-03-20 17:04:30 +0800

    revamp page to simple layout

---
+++ src/Contact.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.section.flex.col.center#contact
+  h1 Contact
+  #icons.flex.center
+    a(href="https://github.com/snowypowers")
+      i.fa.fa-github-square.pri-darken-text
+    a(href="https://www.linkedin.com/in/jun-xiang-yak-7b387687/")
+      i.fa.fa-linkedin-square.pri-darken-text
+    a(href="mailto:yakjunxiang@gmail.com" title="yakjunxiang@gmail.com")
+      i.fa.fa-envelope.pri-darken-text
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#icons
+  font-size: 80px
+  > a
+
+    margin: 16px
+
+</style>
